607c03d151fc4e3f3d29a0f80a384b7d95379428
José <jose.r.goncalves@tecnico.ulisboa.pt>
2026-06-22 20:20:44 +0100

    feat: added CXXX opcode

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,4 +1,5 @@
 use crate::screen;
+use rand::rng;
 
 pub struct Cpu {
     pub mem: [u8; 4096], // memory
@@ -63,6 +64,7 @@ impl Cpu {
             (9, _, _, 0) => if self.v[n2 as usize] != self.v[n3 as usize] {self.pc += 2;},
             (0xA, _, _, _) => self.i = opcode & 0xFFF,                         // set i register
             (0xB, _, _, _) => self.pc = opcode & 0xFFF + self.v[n2 as usize] as u16, 
+            (0xC, _, _, _) => self.v[n2 as usize] = (rand::random_range(0..=0xFF) & (opcode & 0x00FF)) as u8,
             (0xD, _, _, _) => self.draw(n2, n3, n4, buffer),
             _ => println!("No such opcode: {}", opcode),
         }
