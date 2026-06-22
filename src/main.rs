12ef5c54b43e1df670c0855c71c8f720b1df8f6d
José <jose.r.goncalves@tecnico.ulisboa.pt>
2026-06-22 16:37:39 +0100

    feat: added reading of rom to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use minifb::Key;
+use std::fs;
 mod cpu;
 mod screen;
 
 fn main() {
     let mut cpu = cpu::Cpu::new();
     let mut window = screen::new();
-    cpu::Cpu::tick(&mut cpu);
+    
+    let rom = fs::read("roms/IBM Logo.ch8").expect("failed to read ROM");
+    for (i, byte) in rom.iter().enumerate() {
+        cpu.mem[0x200 + i] = *byte;
+    }
+    
     while !window.is_key_down(Key::Escape) {
         screen::update_buffer(&mut window, vec![0; screen::WIDTH * screen::HEIGHT]);
     }
