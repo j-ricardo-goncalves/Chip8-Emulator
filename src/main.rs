4f7109c467a460e239212477f57e19ac92e53e14
José <jose.r.goncalves@tecnico.ulisboa.pt>
2026-06-22 14:16:50 +0100

    feat: added functions for screen and buffer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use minifb::Key;
 mod cpu;
+mod screen;
 
 fn main() {
     let mut cpu = cpu::Cpu::new();
+    let mut window = screen::new();
     cpu::Cpu::tick(&mut cpu);
+    while !window.is_key_down(Key::Escape) {
+        screen::update_buffer(&mut window, vec![0; screen::WIDTH * screen::HEIGHT]);
+    }
 }
--- src/screen.rs
+++ src/screen.rs
@@ -1,11 +1,9 @@
-use minifb::{Key, Window, WindowOptions};
+use minifb::{Window, WindowOptions};
 
-const WIDTH: usize = 640;
-const HEIGHT: usize = 320;
+pub const WIDTH: usize = 640;
+pub const HEIGHT: usize = 320;
 
-pub fn new() {
-    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
-
+pub fn new() -> Window {
     let mut window = Window::new(
         "Chip 8 Emulator/Interpreter",
         WIDTH,
@@ -15,18 +13,11 @@ pub fn new() {
     .unwrap_or_else(|e| {
         panic!("{}", e);
     });
+    window
+}
 
-    // Limit to max ~60 fps update rate
-    window.set_target_fps(60);
-
-    while window.is_open() && !window.is_key_down(Key::Escape) {
-        for i in buffer.iter_mut() {
-            *i = 0; // write something more funny here!
-        }
-
-        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
-        window
-            .update_with_buffer(&buffer, WIDTH, HEIGHT)
-            .unwrap();
-    }
+pub fn update_buffer(window: &mut Window, buffer: Vec<u32>) {
+    window
+        .update_with_buffer(&buffer, WIDTH, HEIGHT)
+        .unwrap();
 }
