c327bedf3fe719fc6bd329200f35ac2210c0c0f1
José <jose.r.goncalves@tecnico.ulisboa.pt>
2026-06-23 21:22:19 +0100

    fix: correct keyboard mapping

--- src/input.rs
+++ src/input.rs
@@ -4,25 +4,25 @@ pub fn get_input(window: &Window, input: &mut [bool; 16]) {
     input.fill(false);
     window.get_keys().iter().for_each(|key|
         match key {
-            Key::Key1 => input[0] = true,
-            Key::Key2 => input[1] = true,
-            Key::Key3 => input[2] = true,
-            Key::Key4 => input[3] = true,
-            
-            Key::Q => input[4] = true,
-            Key::W => input[5] = true,
-            Key::E => input[6] = true,
-            Key::R => input[7] = true,
+            Key::Key1 => input[0x1] = true,
+            Key::Key2 => input[0x2] = true,
+            Key::Key3 => input[0x3] = true,
+            Key::Key4 => input[0xC] = true,
 
-            Key::A => input[8] = true,
-            Key::S => input[9] = true,
-            Key::D => input[10] = true,
-            Key::F => input[11] = true,
+            Key::Q => input[0x4] = true,
+            Key::W => input[0x5] = true,
+            Key::E => input[0x6] = true,
+            Key::R => input[0xD] = true,
 
-            Key::Z => input[12] = true,
-            Key::X => input[13] = true,
-            Key::C => input[14] = true,
-            Key::V => input[15] = true,
+            Key::A => input[0x7] = true,
+            Key::S => input[0x8] = true,
+            Key::D => input[0x9] = true,
+            Key::F => input[0xE] = true,
+
+            Key::Z => input[0xA] = true,
+            Key::X => input[0x0] = true,
+            Key::C => input[0xB] = true,
+            Key::V => input[0xF] = true,
              
             Key::Space => println!("PRESSING SPACE YIPPIE"),
             _ => println!(""),
