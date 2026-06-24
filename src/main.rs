d1fc583a7191b038a6cfe60f99e51f3f4214addb
José <jose.r.goncalves@tecnico.ulisboa.pt>
2026-06-24 11:17:20 +0100

    feat: added pausing and resetting the emulator via space and F5 key

--- src/main.rs
+++ src/main.rs
@@ -17,17 +17,31 @@ fn main() {
     let mut buffer = [0; screen::WIDTH * screen::HEIGHT];
     let mut input = [false; 16];
     let mut window = screen::new();
-    
+    let mut paused = false;
+        
     let args: Vec<String> = std::env::args().collect();
     let rom_path = args.get(1).expect("Usage: Chip8-Emulator <rom_path>");
     read_rom(&mut cpu, rom_path);
     
     while !window.is_key_down(Key::Escape) {
-        input::get_input(&window, &mut input);
-        for _ in 0..10 {
-            cpu.tick(&mut buffer, &input);
+        if window.is_key_pressed(Key::Space, minifb::KeyRepeat::No) {
+            paused = !paused;
         }
-        cpu.tick_timers();
+        if window.is_key_pressed(Key::F5, minifb::KeyRepeat::No) {
+            cpu = cpu::Cpu::new();
+            buffer.fill(screen::OFF);
+            read_rom(&mut cpu, rom_path);
+        }
+        
+        input::get_input(&window, &mut input);
+
+        if !paused {
+            for _ in 0..10 {
+                cpu.tick(&mut buffer, &input);
+            }
+            cpu.tick_timers();
+        }
+        
         screen::update_buffer(&mut window, &buffer);
         std::thread::sleep(time::Duration::from_millis(16)); // ~60fps
     }
