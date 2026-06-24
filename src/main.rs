1fb9627a488708c1678667f5ad508f8f4a421448
José <jose.r.goncalves@tecnico.ulisboa.pt>
2026-06-24 11:09:57 +0100

    feat: added cli rom_path configuration

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,22 @@ mod cpu;
 mod screen;
 mod input;
 
+fn read_rom(cpu: &mut cpu::Cpu, rom_path: &String) {
+    let rom = fs::read(rom_path).expect("failed to read ROM");
+    for (i, byte) in rom.iter().enumerate() {
+        cpu.mem[0x200 + i] = *byte;
+    }
+}
+
 fn main() {
     let mut cpu = cpu::Cpu::new();
     let mut buffer = [0; screen::WIDTH * screen::HEIGHT];
     let mut input = [false; 16];
     let mut window = screen::new();
     
-    let rom = fs::read("roms/IBM Logo.ch8").expect("failed to read ROM");
-    for (i, byte) in rom.iter().enumerate() {
-        cpu.mem[0x200 + i] = *byte;
-    }
+    let args: Vec<String> = std::env::args().collect();
+    let rom_path = args.get(1).expect("Usage: Chip8-Emulator <rom_path>");
+    read_rom(&mut cpu, rom_path);
     
     while !window.is_key_down(Key::Escape) {
         input::get_input(&window, &mut input);
